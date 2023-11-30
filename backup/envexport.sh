d9005c1ce3af914c2984fd7db625dad622e42015
Isaac Lopez <isaac.lopez@okta.com>
2023-11-30 04:27:57 -0600

    remove

--- backup/envexport.sh
+++ backup/envexport.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #set environment variables
-export OKTA_ORG_NAME=demo-stage
+export OKTA_ORG_NAME=okta-name
 export OKTA_BASE_URL=okta.com
-export OKTA_API_TOKEN=003KeT0en3CYHIhvZPoleMBt8_Y40SSzr13Q5WBsVd
+export OKTA_API_TOKEN=003KeT0xxx3Q5WBsVd
