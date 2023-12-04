a50b0605143f7e9e62628970370b520d3ba85387
Isaac Lopez <isaac.lopez@okta.com>
2023-12-04 16:55:48 -0600

    remove

--- clon/envexport.sh
+++ clon/envexport.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #set environment variables
-export OKTA_ORG_NAME=demo-stage
+export OKTA_ORG_NAME=okta-name
 export OKTA_BASE_URL=okta.com
-export OKTA_API_TOKEN=00V6m44VJjI9AJkN3gf85cjz_vpkGWmP_2CTbts3I7
+export OKTA_API_TOKEN=003KeT0xxx3Q5WBsVd
